162a26ef060aa416dc3729668bf82decc77dd4f6
Romesh Perera <91802374+romesh97@users.noreply.github.com>
2024-09-29 19:07:35 +0100

    code changes

--- eular.js
+++ eular.js
@@ -14,25 +14,25 @@ alert('Find the sum of all the multiples of 3 or 5 below 1000 : ' + total);
 a = 3;
 b = 5;
 n = 1000;
-function findMultiples(a, b, n) {
-    total = 0;
-    for (i = 0; i < n; i++) {
-        if (i % a == 0 || i % b == 0) total += i;
-    };
-    return total;
+function sumMultiples(a, b, n) {
+    let sum = 0;
+    for (let i = 1; i < n; i++) {
+        if (i % a === 0 || i % b === 0) {
+            sum += i;
+        }
+    }
+    return sum;
 }
 
-const totalFinal = findMultiples(a, b, n);
-alert('multiples of a or b below n : ' + totalFinal);
+const result = findMultiples(3, 5, 1000);
+alert('multiples of a or b below n : ' + result);
 
 // 3.
-function multiplesValue() {
-    let inputA = document.getElementById('inputA').value;
-    let inputB = document.getElementById('inputB').value;
-    let inputN = document.getElementById('inputN').value;
-    total = 0;
-    for (i = 0; i < inputN; i++) {
-        if (i % inputA == 0 || i % inputB == 0) total += i;
-    };
-    alert('Your answer is ' + total);
+function calculateFromUserInput() {
+    let a = document.getElementById('inputA').value;
+    let b = document.getElementById('inputB').value;
+    let n = document.getElementById('inputN').value;
+
+    const result = sumMultiples(a, b, n);
+    alert(`Sum of multiples of ${a} or ${b} below ${n} is: ${result}`);
 }
