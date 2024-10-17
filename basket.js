23ae1d68dc129ef4789cc9a8a978f01005801e60
Romesh Perera <91802374+romesh97@users.noreply.github.com>
2024-10-18 00:34:18 +0100

    added reduce fnc

--- basket.js
+++ basket.js
@@ -1,16 +1,16 @@
 function calculateBasketTotal(basket, prices) {
     const priceMap = new Map(prices.map(item => [item.product, item.price]));
-    let total = 0;
-    for(const item of basket){
-        const price = priceMap.get(item.product) || 0;
-        total += item.quantity * price
-        console.log("prices are" + price)
-    }
-    return total;
-    // return basket.reduce((total, item) => {
+    // let total = 0;
+    // for(const item of basket){
     //     const price = priceMap.get(item.product) || 0;
-    //     return total + (item.quantity * price);
-    // }, 0);
+    //     total += item.quantity * price
+    //     console.log("prices are" + price)
+    // }
+    // return total;
+    return basket.reduce((total, item) => {
+        const price = priceMap.get(item.product) || 0;
+        return total + (item.quantity * price);
+    }, 0);
 }
 
 const basket = [
