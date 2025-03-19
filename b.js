5d2d53186b31c630882c1e831490bb70817a0c36
Romesh Perera <91802374+romesh97@users.noreply.github.com>
2025-03-19 12:20:24 +0000

    changes

--- b.js
+++ b.js
@@ -1,9 +1,14 @@
-//add alert
-let doIt = () => {
-    inputText = document.getElementById("userInput").value
+// Select the buttons and the paragraph
+const firstButton = document.getElementById('firstButton');
+const secondButton = document.getElementById('secondButton');
+const message = document.getElementById('message');
 
-    if (inputText > 15) {
-        alert("number is exceeding 15");
-    }
+// Add an event listener to the first button
+firstButton.addEventListener('click', () => {
+  message.textContent = 'Hi, Isaani';
+});
 
-}
+// Add an event listener to the second button
+secondButton.addEventListener('click', () => {
+  message.textContent = 'That I have to say you something';
+});
